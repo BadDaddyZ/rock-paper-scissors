ddf0a5fe9cee54fd49ca6ead95a95d19300ef61e
BadDad <baddadz@proton.me>
2022-07-22 15:32:41 -0300

    Scripted computer to play a single round

--- rps.js
+++ rps.js
@@ -1,16 +1,26 @@
-const computerChoice = ['Rock', 'Paper', 'Scissors']
 
-function getComputerChoice()  {
-    const choice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    console.log(choice);
-}
 
-getComputerChoice();
+function rpsMatch() {
+    const computerChoice = ['Rock', 'Paper', 'Scissors']
 
-function getPlayerChoice() {
-    playerChoice = prompt('Choose your weapon - Rock Paper or Scissors');
-    console.log(playerChoice)
-}
+    function getComputerChoice()  {
+        const choice = computerChoice[Math.floor(Math.random() * computerChoice.length)];        
+        console.log(choice);
+        
+        const playerChoice = prompt ('Choose your weapon Rock Paper or Scissors');
+            console.log(playerChoice);
+        
+        if (choice === playerChoice) {
+            alert('Tie');
+        } else if 
+            (choice === 'Rock' && playerChoice === 'Scissors' || choice === 'Scissors' && playerChoice === 'Paper') {
+                alert('Computer wins');        
+        } else if (playerChoice === 'Rock' && choice === 'Scissors' || playerChoice === 'Scissors' && choice === 'Paper') {
+                alert('You Win');
+        }
+    }   
+    getComputerChoice();
 
-getPlayerChoice();
-
+   
+    } 
+rpsMatch();
