cd8fc64ea7889a4bf250e4ba0895c2007ba97fa4
BadDad <baddadz@proton.me>
2022-07-27 08:29:21 -0300

    Coded to allow selection of number of rounds with results of each round console logged.

--- rps.js
+++ rps.js
@@ -1,31 +1,32 @@
 
-function game(
-    for (let i = 0; i < 5; i++) {
-function rpsMatch(playerSelection, computerSelection) {
+function game() {
+let rounds = parseInt(prompt('How many rounds would you like to play'));
+for (let i = 1; i <= rounds; i++) {   
+      
+function playRound(playerSelection, computerSelection) {
     const computerChoice = ['rock', 'paper', 'scissors']
 
     function getComputerChoice()  {
         const computerSelection = computerChoice[Math.floor(Math.random() * computerChoice.length)];        
-        //console.log(computerSelection);
+        console.log(computerSelection);
         
         const playerSelection = prompt ('Choose your weapon Rock Paper or Scissors');
-           //console.log(playerSelection);
-            const playerChoice = (playerSelection.toLowerCase());
-           //console.log(playerChoice);
-                    
+           const playerChoice = (playerSelection.toLowerCase());
+           console.log(playerChoice);
+                
         if (computerSelection === playerChoice) {
-            alert('Tie');
+            console.log('Tie');
         } else if 
-            (computerSelection === 'rock' && playerChoice === 'scissors' || computerSelection === 'scissors' && playerChoice === 'paper') {
-                alert('Computer wins');        
-        } else if (playerChoice === 'rock' && computerSelection === 'scissors' || playerChoice === 'scissors' && computerSelection === 'paper') {
-                alert('You Win');
+            (computerSelection === 'rock' && playerChoice === 'scissors' || computerSelection === 'scissors' && playerChoice === 'paper' || computerSelection === 'paper' && playerChoice === 'rock') {
+            console.log('Computer wins');        
+        } else if (playerChoice === 'rock' && computerSelection === 'scissors' || playerChoice === 'scissors' && computerSelection === 'paper' || playerChoice === 'paper' && computerSelection === 'rock') {
+            console.log('You Win');
         }
+        }
+        getComputerChoice();
         return [playerSelection, computerSelection];
     }   
-     
-     console.log(getComputerChoice(playerSelection, computerSelection)); 
-    } 
-rpsMatch();
-    }
-)
+    playRound();    
+}
+}
+game();
