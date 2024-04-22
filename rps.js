d9e5f95edc05d8d91bfe45eb9a3bf1fbce7b6c0f
Zak McLaren <baddadz@proton.me>
2024-04-22 14:41:16 -0300

    Added buttons to the game. HTML code and JS code updated

--- rps.js
+++ rps.js
@@ -1,29 +1,33 @@
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
-    return choices [Math.floor(Math.random() * choices.length)];
+    return choices[Math.floor(Math.random() * choices.length)];
 }      
-function playRound() {
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
-    const playerSelection = prompt ('Choose your weapon: Rock, Paper, or Scissors').toLowerCase();
-    console.log("Computer selects: " + computerSelection);
-    console.log("Player seelcts: " + playerSelection);
+    const playerChoice = playerSelection.toLowerCase();
+   
+    const resultsDiv = document.getElementById('results');
+    //resultsDiv.innerHTML = 'Player selects: ${playerChoice}<br>Computer selects: ${computerSelection}<br>';
 
-        if (computerSelection === playerSelection) {
-            console.log('Tie');
+
+        if (computerSelection === playerChoice) {
+            resultsDiv.innerHTML += 'It\'s a Tied!';
         } else if 
-            (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'rock') {
-            console.log('Computer wins');        
-        } else {
-            console.log('You Win');
+            (computerSelection === 'rock' && playerChoice === 'scissors' || computerSelection === 'scissors' && playerChoice === 'paper' || computerSelection === 'paper' && playerChoice === 'rock') {
+            resultsDiv.innerHTML += 'Computer wins this round!';
+            } else {
+            resultsDiv.innerHTML += 'You win this round!';
         };
         }
-function game () {
-    let rounds = parseInt(prompt("How many rounds would you like to play?"));
-    for (let i = 1; i <= rounds; i++) {
-        console.log("Round " +i);
-        playRound();
-    }
-}
+//function game () {
+  //  let rounds = parseInt(prompt("How many rounds would you like to play?"));
+    //for (let i = 1; i <= rounds; i++) {
+      //  console.log("Round " +i);
+        //playRound();
+    //}
+//}
 
-game();
+document.getElementById('rock').addEventListener('click', function() { playRound('rock'); });
+document.getElementById('paper').addEventListener('click', function() { playRound('paper'); });
+document.getElementById('scissors').addEventListener('click', function() { playRound('scissors'); });
